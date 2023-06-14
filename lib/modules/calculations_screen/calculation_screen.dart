a790eab0ecb60a846034f9f91d61cc7795c609aa
ammar hammoud <st.ammar.hammoud@gmail.com>
2023-06-14 20:28:08 +0300

    Create a graphic chart with static data

--- lib/modules/calculations_screen/calculation_screen.dart
+++ lib/modules/calculations_screen/calculation_screen.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 class CalculationScreen extends StatelessWidget {
   const CalculationScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+                height: 250,
+                child: SfCartesianChart(
+                  title: ChartTitle(text: 'Calculations'),
+                    primaryXAxis: CategoryAxis(),
+                    series: <LineSeries<SalesData, String>>[
+                      LineSeries<SalesData, String>(
+                          dataSource:  <SalesData>[
+                            SalesData('Sun', 2),
+                            SalesData('Mon', 5),
+                            SalesData('Tue', 1),
+                            SalesData('Wed', 7),
+                            SalesData('Thu', 5),
+                            SalesData('Fri', 6),
+                            SalesData('Sat', 10),
+                          ],
+                          xValueMapper: (SalesData sales, _) => sales.day,
+                          yValueMapper: (SalesData sales, _) => sales.sales
+                      )
+                    ]
+                )
+            )
+        )
+    );
   }
+}
+
+class SalesData {
+  SalesData(this.day, this.sales);
+  final String day;
+  final double sales;
 }
